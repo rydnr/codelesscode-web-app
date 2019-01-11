bcf5f490b16098aa0a8d89434afa2168e6036ab6
Jose San Leandro <jose.sanleandro@osoco.es>
2019-01-11 20:26:35 +0100

    :innocent: Console test passing now

---
+++ src/Console.js
@@ -0,0 +1,11 @@
+// src/Console.js
+import React from 'react';
+import propTypes from 'prop-types';
+
+class Console extends React.PureComponent {
+  render() {
+    return <div />;
+  }
+}
+
+export default Console;
