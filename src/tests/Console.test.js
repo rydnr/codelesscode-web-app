127bd911e0e1acaadde97609a8bc519771877b13
Jose San Leandro <jose.sanleandro@osoco.es>
2019-01-11 20:25:58 +0100

    :test-tube: Initial Console test

---
+++ src/tests/Console.test.js
@@ -0,0 +1,16 @@
+// src/tests/Console.test.js
+import React from 'react';
+import { shallow } from 'enzyme';
+import Console from '../Console';
+
+describe('Console', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Console />);
+  });
+
+  it('renders a <div>', () => {
+    expect(wrapper.type()).toBe('div');
+  });
+});
