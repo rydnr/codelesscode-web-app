e52529258520d2f3b192637a6e578205839fb24b
Jose San Leandro <jose.sanleandro@osoco.es>
2019-01-11 20:52:23 +0100

    :test-tube: Test to make Console include LogWatch

--- src/tests/Console.test.js
+++ src/tests/Console.test.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Console from '../Console';
+import LogWatch from '../LogWatch';
 
 describe('Console', () => {
   let wrapper;
@@ -16,5 +17,9 @@ describe('Console', () => {
 
   it('is initially disabled', () => {
     expect(wrapper.state('enabled')).toBe(false);
+  });
+
+  it('renders a LogWatch with a sample text', () => {
+    expect(wrapper.find(LogWatch).at(0).prop('children')).toBe('LogWatch');
   });
 });
