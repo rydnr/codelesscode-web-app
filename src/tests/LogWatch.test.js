51a57b0aa89c862166c3090adf39daf01cb838cc
Jose San Leandro <jose.sanleandro@osoco.es>
2019-01-16 07:18:44 +0100

    :test-tube: Test to ensure LogWatch can be rendered as a HTML table or as a ReactTable

--- src/tests/LogWatch.test.js
+++ src/tests/LogWatch.test.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import LogWatch from '../LogWatch';
+import LogWatchEntry from '../LogWatchEntry';
 import ReactTable from 'react-table';
 
 describe('LogWatch', () => {
@@ -17,14 +18,28 @@ describe('LogWatch', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<LogWatch>{text}</LogWatch>);
+    wrapper = shallow(
+        <LogWatch>
+          <LogWatchEntry timestamp='2010/03/11 10:30:07'>Starting up</LogWatchEntry>
+          <LogWatchEntry timestamp='2010/03/11 10:31:57'>Accepting requests</LogWatchEntry>
+          <LogWatchEntry timestamp='2018/12/31 23:59:59'>Shutting down</LogWatchEntry>
+        </LogWatch>);
   });
 
-  it('renders a <ReactTable>', () => {
+  it('renders a <table> if htmlTable is enabled', () => {
+    wrapper.setProps({ htmlTable: true });
+    expect(wrapper.type()).toBe('table');
+  });
+
+  it('renders a <ReactTable> by default', () => {
     expect(wrapper.type()).toBe(ReactTable);
   });
 
   it('renders two columns', () => {
     expect(wrapper.prop('columns')).toEqual(columns);
+  });
+
+  it('renders a number of <LogWatchEntry> items', () => {
+    expect(wrapper.children().length).toBe(3);
   });
 });
