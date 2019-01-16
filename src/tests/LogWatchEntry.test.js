6dd912bca2851dca536ad47dfd51798853183711
Jose San Leandro <jose.sanleandro@osoco.es>
2019-01-16 06:20:55 +0100

    :test-tube: LogWatchEntry renders itself as a <tr>

--- src/tests/LogWatchEntry.test.js
+++ src/tests/LogWatchEntry.test.js
@@ -9,7 +9,13 @@ describe('LogWatchEntry', () => {
     wrapper = shallow(<LogWatchEntry />);
   });
 
-  it('renders a <div>', () => {
-    expect(wrapper.type()).toBe('div');
+  it('renders a <tr>', () => {
+    expect(wrapper.type()).toBe('tr');
+  });
+
+  it('renders two <td>s as children', () => {
+    expect(wrapper.children().length).toBe(2);
+    expect(wrapper.childAt(0).type()).toBe('td');
+    expect(wrapper.childAt(1).type()).toBe('td');
   });
 });
