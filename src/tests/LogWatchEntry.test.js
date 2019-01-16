7d99b25fbdc1c888ec2a713fc048b950293aac94
Jose San Leandro <jose.sanleandro@osoco.es>
2019-01-16 06:28:27 +0100

    :test-tube: Test to ensure the first child of LogWatchEntry needs a timestamp

--- src/tests/LogWatchEntry.test.js
+++ src/tests/LogWatchEntry.test.js
@@ -4,9 +4,10 @@ import { shallow } from 'enzyme';
 import LogWatchEntry from '../LogWatchEntry';
 
 describe('LogWatchEntry', () => {
+  const timestamp = '2019/01/16 21:35:17';
   let wrapper;
   beforeEach(() => {
-    wrapper = shallow(<LogWatchEntry />);
+    wrapper = shallow(<LogWatchEntry timestamp={timestamp} />);
   });
 
   it('renders a <tr>', () => {
@@ -17,5 +18,9 @@ describe('LogWatchEntry', () => {
     expect(wrapper.children().length).toBe(2);
     expect(wrapper.childAt(0).type()).toBe('td');
     expect(wrapper.childAt(1).type()).toBe('td');
+  });
+
+  it('passes `timestamp` through to the first child', () => {
+    expect(wrapper.childAt(0).text()).toBe(timestamp);
   });
 });
