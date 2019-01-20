a19c9d45514c2faf9a9a8cdb841bf36050916c32
Jose San Leandro <jose.sanleandro@osoco.es>
2019-01-20 09:51:22 +0100

    :sparkles: Rendering data correctly in Console -> LogWatch -> LogWatchEntry

--- src/Console.js
+++ src/Console.js
@@ -1,14 +1,35 @@
 // src/Console.js
 import React from 'react';
+import PropTypes from 'prop-types';
 import LogWatch from './LogWatch';
+import LogWatchEntry from './LogWatchEntry';
 
 class Console extends React.PureComponent {
+  static propTypes = {
+    logEntries: PropTypes.arrayOf(PropTypes.shape({
+      timestamp: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+    })).isRequired,
+  }
   state = {
     enabled: false,
   };
 
+  renderLogWatchEntries = () => {
+    let entries = [];
+
+    const { logEntries } = this.props;
+
+    logEntries.forEach((each, index) => {
+      const key = 'log-' + index;
+      entries.push(<LogWatchEntry key={key} timestamp={each.timestamp}>{each.text}</LogWatchEntry>);
+    });
+
+    return entries;
+  };
+
   render() {
-    return <div><LogWatch>LogWatch</LogWatch></div>;
+    return (<div><LogWatch>{this.renderLogWatchEntries()}</LogWatch></div>);
   }
 }
 
