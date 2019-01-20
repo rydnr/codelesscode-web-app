fcf77455e6f1b705b5abb286bd6e37d373d9cfa6
Jose San Leandro <jose.sanleandro@osoco.es>
2019-01-20 09:50:08 +0100

    :test-tube: Added sample log entries to Console component, which doesn\'t accept yet

--- example/index.js
+++ example/index.js
@@ -2,7 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Console from '../src/Console';
+import logEntries from './logEntries';
 
 const container = document.createElement('div');
 document.body.appendChild(container);
-ReactDOM.render(<Console/>, container);
+ReactDOM.render(<Console logEntries={logEntries} />, container);
+//
